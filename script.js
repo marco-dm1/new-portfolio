a16f33228d3c2ca54ce1a8e41311e750eca08340
marcodm-1 <marcodiazmoore@gmail.com>
2022-01-11 21:27:28 -0500

    Background video default playback rate change

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function changeDefaultSpeed(){
+    // Changes the default speed of the background video.
+    let backgroundVideo = document.getElementById("BackgroundVideo");
+    backgroundVideo.playbackRate = 0.8;
+}
+
+changeDefaultSpeed();
